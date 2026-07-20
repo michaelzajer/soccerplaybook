52caf5e86f69f75783ce28903b7b3e71310168b6
Michael Zajer <michaelzajer@Michaels-MacBook-Pro.local>
2026-07-20 16:41:57 +1000

    Firebase hosting config and live credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,13 @@
 // Paste your Firebase web app config here.
 // Firebase console -> Project settings -> Your apps -> Web app -> SDK setup and configuration.
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "0000000000",
-  appId: "1:0000000000:web:xxxxxxxxxxxx"
-};
+{
+  "projectId": "soccerplaybook-d2506",
+  "appId": "1:394233153345:web:4a4d75642e6f8205a903c0",
+  "storageBucket": "soccerplaybook-d2506.firebasestorage.app",
+  "apiKey": "AIzaSyBmrrPEVr-3e10alF7IC4MezFMSkOcnwfc",
+  "authDomain": "soccerplaybook-d2506.firebaseapp.com",
+  "messagingSenderId": "394233153345",
+  "measurementId": "G-XC8KDX9R05",
+  "projectNumber": "394233153345",
+  "version": "2"
+}
